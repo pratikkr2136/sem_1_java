808708f7692f9f859d0818eed83d84031ca5fbd4
pratikkr2136 <91750225+pratikkr2136@users.noreply.github.com>
2023-12-27 09:37:58 +0530

    Add files via upload

---
+++ div.java
@@ -0,0 +1,8 @@
+package maths_sikho;
+public class div
+{
+	public void div_1(int a,int b)
+	{
+		System.out.println("division is "+(a/b));
+	}
+}  
---
+++ sub.java
@@ -0,0 +1,8 @@
+package maths_sikho;
+public class sub
+{
+	public void sub_1(int a,int b)
+	{
+		System.out.println("substration = "+(a-b) );
+	}
+}  
