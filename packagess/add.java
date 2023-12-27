ce033072e3637ad232605c60da7f9c33a15f2c12
pratikkr2136 <91750225+pratikkr2136@users.noreply.github.com>
2023-12-27 10:39:12 +0530

    Add files via upload

---
+++ packagess/add.java
@@ -0,0 +1,8 @@
+package maths_sikho;
+public class add
+{
+	public void add_1(int a,int b)
+	{
+		System.out.println("addition is "+(a+b));
+	}
+}  
---
+++ packagess/main.java
@@ -0,0 +1,17 @@
+import maths_sikho.add; 
+import maths_sikho.sub;
+import maths_sikho.div;
+
+public class main{
+	public static void main(String [] args)
+	{
+		add a = new add();
+		a.add_1(10,20);
+
+		sub s=new sub();
+		s.sub_1(30,40);
+
+		div d = new div();
+		d.div_1(10,5);
+	}
+}  
